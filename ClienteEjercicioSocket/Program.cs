d3684c6737f1fd67214333ffc40f99782e63473d
crisrdz <cristofer.rodriguez.elgueta@gmail.com>
2022-04-28 19:34:23 -0400

    Agregar archivos de proyecto.

---
+++ ClienteEjercicioSocket/Program.cs
@@ -0,0 +1,83 @@
+﻿using ClienteEjercicioSocket.Comunicacion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClienteEjercicioSocket
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            
+            bool seguir = false;
+            ClienteSocket clienteSocket=null;
+            string servidor ="";
+            int puerto=0;
+
+            while(!seguir)
+            {
+                try
+                {
+                    Console.WriteLine("Por favor, escriba la IP del servidor al cual conectarse: ");
+                    servidor = Console.ReadLine();
+                    Console.WriteLine("Por favor, escriba el puerto al cual conectarse: ");
+                    puerto = Convert.ToInt32(Console.ReadLine());
+                    clienteSocket = new ClienteSocket(servidor, puerto);
+                    if(clienteSocket.Conectar())
+                    {
+                        seguir = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error de comunicación");
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Por favor ingrese datos válidos");
+                }
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Conectado a Servidor {0} en puerto {1}", servidor, puerto);
+            
+            Console.WriteLine("Escriba su mensaje a continuación");
+            Comunicacion(clienteSocket);
+            
+            Console.ReadKey();
+        }
+
+        public static void Comunicacion(ClienteSocket cliente)
+        {
+            while (true)
+            {
+                Console.Write("Cliente: ");
+                string mensajeCliente = Console.ReadLine().Trim();
+                cliente.Escribir(mensajeCliente);
+
+                if (mensajeCliente.ToLower() == "chao")
+                {
+                    cliente.Desconectar();
+                    Console.WriteLine("Desconectado");
+                    break;
+                }
+
+                string mensajeServidor = cliente.Leer();
+                Console.WriteLine("Servidor: {0}", mensajeServidor);
+                if (mensajeServidor.ToLower() == "chao")
+                {
+                    cliente.Desconectar();
+                    Console.WriteLine("Desconectado");
+                    break;
+                }
+                
+            }
+            
+        }
+    }
+
+}
---
+++ EjercicioSockets/Program.cs
@@ -0,0 +1,66 @@
+﻿using EjercicioSocketsUtils;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioSocket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int puerto = Convert.ToInt32(ConfigurationManager.AppSettings["puerto"]);
+            Console.WriteLine("Iniciando Servidor en puerto {0}", puerto);
+            ServerSocket servidor = new ServerSocket(puerto);
+
+            if (servidor.Iniciar())
+            {
+                Console.WriteLine("Servidor iniciado");
+                while (true)
+                {
+                    Console.WriteLine("Esperando cliente");
+                    Socket socketCliente = servidor.ObtenerCliente();
+
+                    ClienteCom cliente = new ClienteCom(socketCliente);
+                    
+                    Console.WriteLine("Cliente conectado, esperando mensaje de cliente...");
+
+                    //Para Hercules:   <CR><LF>
+
+                    string mensajeCliente, mensajeServidor;
+                    while (true)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        mensajeCliente = cliente.Leer();
+                        if(mensajeCliente.Trim().ToLower() == "chao")
+                        {
+                            Console.WriteLine("Cliente dijo 'chao', desconectando...");
+                            break;
+                        }
+                        Console.WriteLine("Cliente: {0}", mensajeCliente);
+                        Console.Write("Servidor: ");
+                        mensajeServidor = Console.ReadLine();
+                        
+                        cliente.Escribir(mensajeServidor);
+                        if (mensajeServidor.Trim().ToLower() == "chao")
+                        {
+                            break;
+                        }
+
+                    }
+
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    cliente.Desconectar();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error, el puerto {0} está en uso...", puerto);
+            }
+        }
+    }
+}
